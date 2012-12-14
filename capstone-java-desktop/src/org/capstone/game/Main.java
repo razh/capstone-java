fcdd9cfe09430af949ac3b832df8f6f38d9121fd
Raymond Zhou <somecrazyidiot@gmail.com>
2012-12-14 12:25:06 -0500

    Reset resolution to default.

--- capstone-java-desktop/src/org/capstone/game/Main.java
+++ capstone-java-desktop/src/org/capstone/game/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "capstone-java";
 		cfg.useGL20 = true;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 1280;
+		cfg.height = 800;
 		cfg.samples = 4;
 		cfg.useCPUSynch = false;
 		
